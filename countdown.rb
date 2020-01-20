9b438f7cdd630d7feac6b3660d27ef84c62f702b
Chloe Gell <gellert.chloe@gmail.com>
2020-01-20 17:36:10 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,6 +5,14 @@ def countdown(int)
     puts "#{int} SECOND(S)!"
     int -= 1 
   end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(int)
+  while int > 0  
+    puts "#{int} SECOND(S)!"
+    int -= 1 
+  end
   return "HAPPY NEW YEAR!"
 end
 
